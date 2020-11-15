351358a232c9dc0db9959d62430250663821eb44
MichalKulygin <michal.kulygin@gmail.com>
2020-11-15 11:24:01 +0100

    init commit

---
+++ src/main/java/com/sda/javagdy4/springs/students_demo/StudentsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sda.javagdy4.springs.students_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/javagdy4/springs/students_demo/StudentsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.javagdy4.springs.students_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
