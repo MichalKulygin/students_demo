590cca9fa862ff0183a627e5092b1d4a2089f78a
MichalKulygin <michal.kulygin@gmail.com>
2020-11-15 13:54:51 +0100

    Committing grade controller

---
+++ src/main/java/com/sda/javagdy4/springs/students_demo/controller/GradeController.java
@@ -0,0 +1,46 @@
+package com.sda.javagdy4.springs.students_demo.controller;
+
+import com.sda.javagdy4.springs.students_demo.model.Grade;
+import com.sda.javagdy4.springs.students_demo.model.GradeSubject;
+import com.sda.javagdy4.springs.students_demo.model.Student;
+import com.sda.javagdy4.springs.students_demo.service.GradeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/grade")
+@RequiredArgsConstructor
+public class GradeController {
+
+    private final GradeService gradeService;
+
+    //############## FORM ##############\\
+    @GetMapping("/form")
+    public String getGradeForm(Model model, @RequestParam("studentId") Long studentId){
+        model.addAttribute("addedGrade", new Grade());
+        model.addAttribute("allSubjects", GradeSubject.values());
+        model.addAttribute("studentId", studentId);
+        return "grade_form";
+    }
+
+    @PostMapping("")
+    public String submitFormData(Grade grade, @RequestParam("studentIdParam") Long studentId){
+        Optional<Student> studentOptional = gradeService.findById(studentId);
+        if(studentOptional.isPresent()) {
+            Student student = studentOptional.get();
+            grade.setStudent(student);
+            gradeService.save(grade);
+        }
+        return "redirect:/student/" + studentId;
+    }
+    //############## DELETE ##############\\
+    //############## GET ##############\\
+    //############## LIST = STUDENT DETAILS = DONE ##############\\
+}
---
+++ src/main/java/com/sda/javagdy4/springs/students_demo/service/GradeService.java
@@ -0,0 +1,25 @@
+package com.sda.javagdy4.springs.students_demo.service;
+
+import com.sda.javagdy4.springs.students_demo.model.Grade;
+import com.sda.javagdy4.springs.students_demo.model.Student;
+import com.sda.javagdy4.springs.students_demo.repository.GradeRepository;
+import com.sda.javagdy4.springs.students_demo.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class GradeService {
+    private final GradeRepository gradeRepository;
+    private final StudentRepository studentRepository;
+
+    public Optional<Student> findById(Long studentId) {
+        return studentRepository.findById(studentId);
+    }
+
+    public void save(Grade grade) {
+        gradeRepository.save(grade);
+    }
+}
